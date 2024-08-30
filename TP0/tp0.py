f011af9c32eeaea358bea55d1ce740cd4bdc9d78
JuanR547 <95152166+JuanR547@users.noreply.github.com>
2024-08-30 12:47:18 -0300

    Interfaz tp0

--- TP0/tp0.py
+++ TP0/tp0.py
@@ -1,10 +1,14 @@
+#python -m pip install -U pip
+#python -m pip install -U matplotlib
+#pip install imagio
+
 import tkinter
 from tkinter import filedialog
 import matplotlib.pyplot as plt
-import imageio
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import imageio.v2 as imageio
 
 # Metodos
-
 rutaImagen = None
 
 def importarImagen():
@@ -15,39 +19,48 @@ def importarImagen():
     )
     if rutaImagen:
         etiqueta["text"] = "Imagen importada correctamente!"
-        print(rutaImagen)
 
 def mostrarImagen():
     global rutaImagen
-    print(rutaImagen)
+    canvas = None
     if rutaImagen:
         imagen = imageio.imread(rutaImagen)
         
-        plt.imshow(imagen)
-        plt.show()
+        fig, ax = plt.subplots(figsize=(4, 2))
+        ax.imshow(imagen)
+        ax.set_title("Imagen Original")
+        ax.axis('off')
+
+        if canvas:
+            canvas.get_tk_widget().destroy()
+
+        canvas = FigureCanvasTkAgg(fig, master=frameImagenOriginal)
+        canvas.get_tk_widget().pack(expand=True)
+        canvas.draw()
+         
     else:
         etiqueta["text"] = "Primero debes cargar una imagen!"
         
 def procesarImagen():
     global rutaImagen
+    canvasProcesado = None
     if rutaImagen:
+        
         imagen = imageio.imread(rutaImagen)
-
-        imagenGris = imageio.core.util.Array(imagen.mean(axis=-1))
         
         imagenRojo = imagen[:, :, 0]
         imagenVerde = imagen[:, :, 1]
         imagenAzul = imagen[:, :, 2]
         
-        fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+        fig, axs = plt.subplots(1, 4, figsize = (12, 2))
 
-        axs[0].imshow(imagenGris, cmap='gray')
+        axs[0].imshow(imagen[:, :, 0], cmap='gray')
         axs[0].set_title("Blanco y Negro")
         axs[0].axis('off')
         
         axs[1].imshow(imagenRojo, cmap='Reds')
         axs[1].set_title("Canal Rojo")
-        axs[1].axis('off')
+        axs[1].axis('off')   
         
         axs[2].imshow(imagenVerde, cmap='Greens')
         axs[2].set_title("Canal Verde")
@@ -57,27 +70,47 @@ def procesarImagen():
         axs[3].set_title("Canal Azul")
         axs[3].axis('off')
         
-        plt.show()
+        if canvasProcesado:
+            canvasProcesado.get_tk_widget().destroy()
+
+        canvasProcesado = FigureCanvasTkAgg(fig, master=frameImagenProcesada)
+        canvasProcesado.get_tk_widget().pack(expand=True)
+        canvasProcesado.draw()
+        
     else:
         etiqueta["text"] = "Primero debes cargar una imagen!"
 
 # Tkinter
 ventana = tkinter.Tk()
+ventana.geometry("1200x700")
+ventana.resizable(False, False)
+
+# Crear un marco para la imagen original
+frameImagenOriginal = tkinter.Frame(ventana, width=960, height=250, bg="white")
+frameImagenOriginal.pack(pady=10)
+
+# Crear un marco para la imagen procesada
+frameImagenProcesada = tkinter.Frame(ventana, width=960, height=250, bg="white")
+frameImagenProcesada.pack(pady=10)
+
+# Crear un frame para los botones y la etiqueta
+frameControles = tkinter.Frame(ventana)
+frameControles.pack(pady=10)
 
 # Crear un botón para cargar la imagen
-botonCargar = tkinter.Button(ventana, text="Cargar Imagen", command=importarImagen)
-botonCargar.pack()
+botonCargar = tkinter.Button(frameControles, text="Cargar Imagen", command=importarImagen)
+botonCargar.grid(row=0, column=0, padx=10)
 
-# Crear un botón para mostrar la imagen 
-botonMostrar = tkinter.Button(ventana, text="Mostrar Imagen", command=mostrarImagen)
-botonMostrar.pack()
+# Crear un botón para mostrar la imagen
+botonMostrar = tkinter.Button(frameControles, text="Mostrar Imagen", command=mostrarImagen)
+botonMostrar.grid(row=0, column=1, padx=10)
 
 # Crear un botón para procesar la imagen
-botonProcesar = tkinter.Button(ventana, text="Procesar Imagen", command=procesarImagen)
-botonProcesar.pack()
+botonProcesar = tkinter.Button(frameControles, text="Procesar Imagen", command=procesarImagen)
+botonProcesar.grid(row=0, column=2, padx=10)
 
-# Etiqueta
-etiqueta = tkinter.Label(ventana)
-etiqueta.pack()
+# Etiqueta para mostrar mensajes de estado
+etiqueta = tkinter.Label(frameControles, text="")
+etiqueta.grid(row=1, columnspan=3, pady=3)
 
 ventana.mainloop()
